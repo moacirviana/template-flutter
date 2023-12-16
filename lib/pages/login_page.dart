acfaa35856aea5d66905612d8f3763f6b7d5771f
Moacir Viana <moacir.viana@gmail.com>
2023-12-16 18:10:43 -0400

    tela de login com validator

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({super.key});
+  final _tLogin = TextEditingController(text: "015697172275");
+  final _tSenha = TextEditingController(text: "123123");
+   //final _tLogin = TextEditingController();
+  //final _tSenha = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+  LoginPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -16,35 +21,45 @@ class LoginPage extends StatelessWidget {
   }
 
   _body() {
-    return Container(
-      padding: const EdgeInsets.all(16),
-      child: ListView(
-        children: [
-          _text("Login", "Digite o login"),
-          const SizedBox(
-            height: 10,
-          ),
-          _text("Senha", "Digite a senha", pwdtext: true),
-          const SizedBox(
-            height: 20,
-          ),
-          SizedBox(
-            height: 46,
-            child: ElevatedButton(
-              child: const Text(
-                "Login",
-                style: TextStyle(color: Colors.white, fontSize: 22),
-              ),
-              onPressed: () {},
+    return Form(
+      key: _formKey,
+      child: Container(
+        padding: const EdgeInsets.all(16),
+        child: ListView(
+          children: [
+            _text("Login", "Digite o login", controller: _tLogin, validator: _validateLogin),
+            const SizedBox(
+              height: 10,
             ),
-          )
-        ],
+            _text("Senha", "Digite a senha", pwdtext: true, controller:_tSenha, validator: _validateSenha),
+            const SizedBox(
+              height: 20,
+            ),
+            SizedBox(
+              height: 46,
+              child: _button("Login", _onClickLogin), 
+            )
+          ],
+        ),
       ),
     );
   }
 
-  TextFormField _text(String label, String hint, {bool pwdtext = false}) {
+  _button(String texto, onPressed) {
+    return ElevatedButton(
+        onPressed: onPressed,
+            child: Text(
+              texto,
+              style: const TextStyle(color: Colors.white, fontSize: 22),
+            ),
+          );
+  }
+
+  TextFormField _text(String label, String hint, {bool pwdtext = false, 
+  TextEditingController ?controller, FormFieldValidator<String> ?validator}) {
     return TextFormField(
+      controller: controller,
+      validator: validator,
       obscureText: pwdtext,
       decoration: InputDecoration(
           labelText: label,
@@ -56,4 +71,28 @@ class LoginPage extends StatelessWidget {
       ),
     );
   }
+
+  _onClickLogin() {
+     if (!_formKey.currentState!.validate()){
+      return;
+    }
+    debugPrint("Login_____");
+  }
+
+  String? _validateLogin(String? value) {
+     if(value != null && value.isEmpty){
+         return "Digite o login";
+     }return null; 
+  }
+
+  String? _validateSenha(String? value) {
+     if(value != null && value.isEmpty){
+         return "Digite a senha";
+     }
+      if(value!.length <3){
+         return "A senha precisa ter pelo menos 3 caracteres.";
+     }
+     return null; 
+  }
+
 }
