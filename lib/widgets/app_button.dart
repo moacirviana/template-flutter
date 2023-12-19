a7dfcc052eefdde04a790909f5361ab8eb0d545b
Moacir Viana <moacir.viana@tre-am.jus.br>
2023-12-19 09:45:03 -0400

    correcao no nome do atributo

--- lib/widgets/app_button.dart
+++ lib/widgets/app_button.dart
@@ -1,25 +1,27 @@
 import 'package:flutter/material.dart';
 
-
 class AppButton extends StatelessWidget {
   //final VoidCallback? onPressed;
   final Function()? onClick;
   final String texto;
-  final bool showProress;
-   const AppButton(this.texto, this.onClick, {super.key, this.showProress=false});
+  final bool showProgress;
+  const AppButton(this.texto, this.onClick,
+      {super.key, this.showProgress = false});
 
   @override
   Widget build(BuildContext context) {
-     return ElevatedButton(
-        onPressed: onClick,
-            child: showProress ? const Center(child: CircularProgressIndicator(
-              valueColor: AlwaysStoppedAnimation(Colors.white),
-            ),)
-            :
-            Text(
+    return ElevatedButton(
+      onPressed: onClick,
+      child: showProgress
+          ? const Center(
+              child: CircularProgressIndicator(
+                valueColor: AlwaysStoppedAnimation(Colors.white),
+              ),
+            )
+          : Text(
               texto,
               style: const TextStyle(color: Colors.white, fontSize: 22),
             ),
-          );
+    );
   }
 }
