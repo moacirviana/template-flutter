efc4e79f3a9cebdb3c7d91801d7aa3e9e13c3046
Moacir Viana <moacir.viana@tre-am.jus.br>
2023-12-19 09:50:08 -0400

    add foto do carro

--- lib/pages/carros/carros_detalhes.dart
+++ lib/pages/carros/carros_detalhes.dart
@@ -17,8 +17,24 @@ class CarrosDetalhes extends StatelessWidget {
   }
 
   _body() {
-    return Center(
-      child: Text(carro.descricao!),
+    return Container(
+      padding: const EdgeInsets.all(10),
+      child: Center(
+        child: Column(
+          children: [
+            Center(
+              child: Image.network(
+                  carro.urlFoto ??
+                      "https://e7.pngegg.com/pngimages/462/631/png-clipart-sports-car-black-and-white-car-compact-car-car-accident-thumbnail.png",
+                  width: 350, errorBuilder: (BuildContext context,
+                      Object exception, StackTrace? stackTrace) {
+                return const Text('😢 image not found');
+              }),
+            ),
+            Text(carro.descricao!),
+          ],
+        ),
+      ),
     );
   }
 }
